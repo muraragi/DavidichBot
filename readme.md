e5de48325c087295857f06788702d979f40ce499
muraragi <muraragi99@gmail.com>
2018-08-03 12:26:55 +0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+###DavidichBot
+A simple discord bot using discord.js
+
+###Instalation
+Clone repo, run npm install.
+Also you need to create auth.json file in cfg folder and paste your token key value here
---
+++ src/bot.js
@@ -0,0 +1,33 @@
+// Import the discord.js module
+const Discord = require('discord.js');
+const auth    = require('../cfg/auth.json');
+const moment  = require('moment');
+
+const jailDate = moment([2016, 02, 19]);
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+// The token of your bot - https://discordapp.com/developers/applications/me
+const token = auth.token;
+
+// The ready event is vital, it means that your bot will only start reacting to information
+// from Discord _after_ ready is emitted
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'justice') {
+    // Send "pong" to the same channel
+    let now = moment();
+    let timePassed = now.diff(jailDate, 'days');
+    let response = `Эрик в заключении уже ${timePassed} дней...`;
+    message.channel.send(response);
+  }
+});
+
+// Log our bot in
+client.login(token);
